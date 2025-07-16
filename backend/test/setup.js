129a2ef9010c271530277d3dccd7c0b0b3e225b1
Hamza Bashir <hmzabshr120@gmail.com>
2025-07-16 10:23:47 +0500

    Something change

--- backend/test/setup.js
+++ backend/test/setup.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // So .env values are available
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URI , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGO_URI );
 });
 
 afterAll(async () => {
