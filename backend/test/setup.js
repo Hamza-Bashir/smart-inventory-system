493e4e64e657887b2adf01baa71714e9f0c76d4e
Hamza Bashir <hmzabshr120@gmail.com>
2025-07-16 11:36:31 +0500

    Something chnage

--- backend/test/setup.js
+++ backend/test/setup.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); // So .env values are available
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(" Connected to MongoDB");
+  } catch (error) {
+    console.error(" MongoDB connection error:", error.message);
+  }
 });
 
 afterAll(async () => {
