e6a11ba948f4c0fcd400dde04af9a2100f5533ec
Hamza Bashir <hmzabshr120@gmail.com>
2025-07-16 15:26:09 +0500

    Login api testing complete

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -73,3 +73,68 @@ describe('POST /api/v1/register-user', () => {
     expect(res.body.error).toBe("password is required")
   })
 });
+
+
+// -------------- user login api test ...............
+
+describe("POST /api/v1/login-user", () => {
+  it("should login user successfully", async () => {
+    const res = await request(app)
+    .post("/api/v1/login-user")
+    .send({
+      email:"maryam@gmail.com",
+      password:"12345"
+    })
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.success).toBe(true)
+    expect(res.body.message).toBe('Login Successfully')
+  })
+
+  it("should fail if email is missing", async ()=>{
+    const res = await request(app)
+    .post("/api/v1/login-user")
+    .send({
+      password:"123"
+    })
+
+    expect(res.status).toBe(400)
+    expect(res.body.error).toBe("email is required")
+  })
+
+  it("should fail if email is invalid", async ()=>{
+    const res = await request(app)
+    .post("/api/v1/login-user")
+    .send({
+      email:"maryam",
+      password:"12345"
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("email must be valid email")
+  })
+
+  it("should fail if password is missing", async () => {
+    const res = await request(app)
+    .post("/api/v1/login-user")
+    .send({
+      email:"maryam@gmail.com"
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("password is required")
+  })
+
+  it("should fail if password cannot string", async () => {
+    const res = await request(app)
+    .post("/api/v1/login-user")
+    .send({
+      email:"maryam@gmail.com",
+      password:12345
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("password must be string")
+  })
+
+})
