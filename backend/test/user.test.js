2efb0961f2da81436a12e4068da9d68c003a43a6
Hamza Bashir <hmzabshr120@gmail.com>
2025-07-16 14:37:24 +0500

    User register api testing complete

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 const app = require('../app'); // Make sure app.js exports express app
 
+
+
+// -------------- user register api test ...............
+
 describe('POST /api/v1/register-user', () => {
   it('should register a new user successfully', async () => {
     const res = await request(app)
@@ -30,5 +34,42 @@ describe('POST /api/v1/register-user', () => {
     })
     expect(res.statusCode).toBe(400)
     expect(res.body.error).toBe("name is required")
+  })
+
+  it("should fail if email is missing", async () => {
+    const res = await request(app)
+    .post("/api/v1/register-user")
+    .send({
+      name:"Hamza",
+      password:"12345"
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("email is required")
+  })
+
+  it("should fail if email is invalid", async () => {
+    const res = await request(app)
+    .post("/api/v1/register-user")
+    .send({
+      name:"Hamza",
+      email:"hmza",
+      password:"12345"
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("email must be valid email")
+  })
+
+  it("should fail if password is missing",async ()=>{
+    const res = await request(app)
+    .post("/api/v1/register-user")
+    .send({
+      name:"Hamza",
+      email:"hmza@gmail.com"
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe("password is required")
   })
 });
